415276b5c7835b14d99f3eedf2fa5a3e24b3b442
Aryan <aryancr72000@gmail.com>
2024-02-26 18:40:47 +0530

    before adding block in v2(gpt)

--- v2.py
+++ v2.py
@@ -20,10 +20,10 @@ n=int(0.9*len(data))
 train=data[:n]
 test=data[n:]
 
-
-
-
+iters=5000
+batch_size=32
 block_size=8
+n_embd=32
 
 def get_batch(split):
     data=train if split=="train" else test 
@@ -49,12 +49,71 @@ def estimate_loss():
     return out
 
 
-class BigramLanguageModel(nn.Module):
-    def __init__(self,vocab_size):
+class Head(nn.Module):
+    def __init__(self,head_size):
         super().__init__()
-        self.token_embedding_table=nn.Embedding(vocab_size,vocab_size)
+        self.key=nn.Linear(n_embd,head_size,bias=False)
+        self.query=nn.Linear(n_embd,head_size,bias=False)
+        self.value=nn.Linear(n_embd,head_size,bias=False)
+        self.register_buffer('tril',torch.tril(torch.ones(block_size,block_size)))
+
+    def forward(self,x):
+        B,T,C=x.shape
+        k=self.key(x)
+        q=self.query(x)
+        v=self.value(x)
+        wei=q@k.transpose(-2,-1) / C**0.5
+        wei=wei.masked_fill(self.tril[:T,:T]==0,float('-inf'))
+        wei=F.softmax(wei,dim=-1)
+        out=wei@v 
+        return out 
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self,num_heads,head_size):
+        super().__init__()
+        self.heads=nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self,x):
+        return torch.cat([h(x) for h in self.heads],dim=-1)
+
+class FeedForward(nn.Module):
+    def __init__(self,n_embd):
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Linear(n_embd,n_embd),
+            nn.ReLU(n_embd),
+        )
+    def forward(self,x):
+        return self.net(x)
+
+class Block(nn.Module):
+    def __init__(self,n_embd,n_head):
+        super().__init()
+        head_size=n_embd//n_head
+        self.sa=MultiHeadAttention(n_head,head_size)
+        self.ffwd=FeedForward(n_embd)
+    def forward(self,x):
+        x=self.sa(x)
+        x=self.ffwd(x)
+        return x
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table=nn.Embedding(vocab_size,n_embd)
+        self.pos_embedding_table=nn.Embedding(block_size,n_embd)
+        self.sa_head=MultiHeadAttention(4,n_embd//4)
+        self.ffwd=FeedForward(n_embd)
+        self.lm_head=nn.Linear(n_embd,vocab_size)
     def forward(self,idx,targets=None):
-        logits=self.token_embedding_table(idx)
+        B,T=idx.shape
+        tok_emb=self.token_embedding_table(idx)
+        pos_emb=self.pos_embedding_table(torch.arange(T,device=device))
+        x=tok_emb+pos_emb 
+        x=self.sa_head(x)
+        x=self.ffwd(x)
+        logits=self.lm_head(x)
+
         if targets is None:
             loss=None 
         else:
@@ -62,23 +121,22 @@ class BigramLanguageModel(nn.Module):
             logits=logits.view(B*T,C)
             targets=targets.view(B*T)
             loss=F.cross_entropy(logits,targets)
-
         return logits,loss 
     
     def generate(self,idx,max_new_token):
         for _ in range(max_new_token):
-            logits,loss=self(idx)
+            idx_cond=idx[:,-block_size:]
+            logits,loss=self(idx_cond)
             logits=logits[:,-1,:]
             probs=F.softmax(logits,dim=-1)
             idx.next=torch.multinomial(probs,num_samples=1)
             idx=torch.cat((idx,idx.next),dim=1)
         return idx
     
-model=BigramLanguageModel(vocab_size)
+model=BigramLanguageModel()
 m=model.to(device)
 optimizer=torch.optim.AdamW(m.parameters(),lr=1e-3)
-batch_size=32
-for steps in range(10000):
+for steps in range(iters):
     if steps%1000==0:
         losses=estimate_loss()
         print(losses)
@@ -88,6 +146,5 @@ for steps in range(10000):
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
-# print(loss.item())
 context=torch.zeros((1,1),dtype=torch.long,device=device)
 print(decode(m.generate(context,max_new_token=500)[0].tolist()))
